f4a06cc62acf48a4909dad697ca1047246da61f9
Deptoha <tohakhan249156@gmail.com>
2024-03-25 21:50:23 +0600

    added footer and navbar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,34 @@
-
+import facebook from '../Images/icons8-facebook-30.png'
+import twitter from '../Images/icons8-twitter-30.png'
+import instagram from '../Images/icons8-instagram-30.png'
 
 const Footer = () => {
     return (
         <div>
-            this is footer
+            <div className="mt-10">
+
+                <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded mt-20 md:container md:mx-auto">
+                    <nav>
+                        <h3 className="font-black text-4xl">Recipe Calories</h3>
+                    </nav>
+                    <nav>
+                        <p className="text-gray-500">Explore our online restaurant where every dishs calorie count is meticulously <br /> provided for guilt-free indulgence.
+                        </p>
+                    </nav>
+                    <nav>
+                        <div className="grid grid-flow-col gap-4">
+                            <img className=' cursor-pointer' src={facebook} alt="" />
+                            <img className=' cursor-pointer' src={twitter} alt="" />
+                            <img className=' cursor-pointer' src={instagram} alt="" />
+                        </div>
+                    </nav>
+                    <aside>
+                        <div className="lg:w-[825px] w-[215px] h-1 bg-[#1313181A]"></div>
+                        <p className="text-gray-500 mt-7">2017, All Rights Reserved.</p>
+                    </aside>
+                </footer>
+
+            </div>
         </div>
     );
 };
