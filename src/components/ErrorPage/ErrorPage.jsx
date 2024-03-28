ae01c070b6eda947f40be323caffebec24f626e1
Deptoha <tohakhan249156@gmail.com>
2024-03-28 23:37:14 +0600

    added full assignment

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -2,8 +2,8 @@
 const ErrorPage = () => {
     return (
         <div className="text-center mt-52">
-            <h1 className="text-6xl text-center mt-52">Not found page</h1>
-            <button to="/ListedBooks" className="btn btn-primary mt-10 font-bold">Go Home</button>
+            <h1 className="text-6xl text-center mt-5s2">Not found page</h1>
+            <button to="/" className="btn btn-primary mt-10 font-bold">Go Home</button>
         </div>
     );
 };
